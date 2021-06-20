5dd4c068ee27ac3bf6ffa5a9fee1881098156a08
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-20 10:44:52 -0400

    setup cookies 50%

--- scripts.js
+++ scripts.js
@@ -2,3 +2,17 @@
 window. Alert = function(message) {
     alert(message);
 }
+window.methods = {
+    CreateCookie: function (name, value, days) {
+     var expires;
+     if (days) {
+        var date = new Date();
+         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          expires = "; expires=" + date.toGMTString();
+     }
+      else {
+         expires = "";
+     }
+      document.cookie = name + "=" + value + expires + "; path=/";
+   }    
+}

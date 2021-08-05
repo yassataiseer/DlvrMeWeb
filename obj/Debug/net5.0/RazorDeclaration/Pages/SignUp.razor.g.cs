767c966e32ea4106b418d899a3bc7984dc5e6905
Yassa Taiseer <yassataiseer@gmail.com>
2021-08-04 20:43:14 -0400

    done

--- obj/Debug/net5.0/RazorDeclaration/Pages/SignUp.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/SignUp.razor.g.cs
@@ -76,42 +76,42 @@ using DlvrMeWeb.Shared;
 #line hidden
 #nullable disable
 #nullable restore
-#line 41 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 44 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 42 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 45 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 using System.Text;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 43 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 46 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 44 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 47 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 using System.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 45 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 48 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 using Newtonsoft.Json.Linq;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 46 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 49 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 using Newtonsoft.Json;
 
 #line default
@@ -126,7 +126,7 @@ using Newtonsoft.Json;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 50 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
+#line 53 "/Users/yassa/DlvrMeWeb/Pages/Signup.razor"
 
     private string Username;
     private string Password;
